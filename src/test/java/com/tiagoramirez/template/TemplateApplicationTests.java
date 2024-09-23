0bba337f4ca60621d7e469c22996ab8bc73d1fe9
Tiago Ramirez <tiagoramirez2001@gmail.com>
2024-09-23 01:25:50 -0300

    first commit

---
+++ src/test/java/com/tiagoramirez/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagoramirez.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
