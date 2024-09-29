fd84a69380d4cd103d6e81774b33b7a98c766440
Tiago Ramirez <tiagoramirez2001@gmail.com>
2024-09-28 22:00:25 -0300

    Added environment module

---
+++ src/main/java/com/tiagoramirez/template/environment/services/EnvironmentService.java
@@ -0,0 +1,24 @@
+package com.tiagoramirez.template.environment.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnvironmentService {
+
+    @Autowired
+    private Environment env;
+
+    public String getProfile() {
+        return env.getProperty("spring.profiles.active");
+    }
+
+    public String getSecretKey1() {
+        return env.getProperty("SECRET_KEY_1");
+    }
+
+    public String getSecretKey2() {
+        return env.getProperty("SECRET_KEY_2");
+    }
+}
